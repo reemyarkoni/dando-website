26879040c644e64125ef57659520b2c877868833
Reem Yarkoni <reem.yarkoni@gmail.com>
2022-11-29 12:07:43 +0200

    starting work on layoutcss

--- src/Components/HomeArea/Home/Home.tsx
+++ src/Components/HomeArea/Home/Home.tsx
@@ -3,7 +3,7 @@ import "./Home.css";
 function Home(): JSX.Element {
     return (
         <div className="Home">
-			<h1>HOME</h1>
+            
         </div>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -9,10 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    
       <Layout />
-    
-    
   </React.StrictMode>
 );
 
