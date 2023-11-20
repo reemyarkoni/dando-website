611d101ec190055dbaab9f85a5fdda3f8fa974c4
Reem Yarkoni <reem.yarkoni@gmail.com>
2023-11-20 15:50:57 +0200

    adding Material bottom-sheet and Dialog, stopping tracking on .angular folder and adding bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
+import { BottomSheetComponent } from './bottom-sheet/bottom-sheet.component';
+import { PopupComponent } from './popup/popup.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dando-website';
+    // constructor(private dialogRef: MatDialog){};
+
+    // openDialog(){
+    //     this.dialogRef.open(PopupComponent);
+    // }
+
+
+  constructor(private bottomSheet: MatBottomSheet){}
+
+  openBottomSheet(){
+    this.bottomSheet.open(BottomSheetComponent);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet'
+import { MatDialogModule } from '@angular/material/dialog';
+import { BottomSheetComponent } from './bottom-sheet/bottom-sheet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BottomSheetComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatDialogModule,
+    NgbModule,
+    MatBottomSheetModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
